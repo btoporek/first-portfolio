3975d305fc7480806e2fe50e6466520d42e08b34
Brittni <brittni.toporek@gmail.com>
2023-04-02 13:08:33 -0700

    changed hero background

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import "../styles/Home.css";
-import desertvideo from "../assets/desertvideo.mp4";
+import backgroundvideo from "../assets/typing4.mp4";
 import React from "react";
 
 function Home() {
   return (
     <>
       <div className="hero-background">
-        <video autoPlay loop muted playsInline id="hero-video">
-          <source src={desertvideo} type="video/mp4" />
+        <video autoPlay loop muted playsInline spee id="hero-video">
+          <source src={backgroundvideo} type="video/mp4" />
         </video>
 
         <div className="hero-overlay"></div>
